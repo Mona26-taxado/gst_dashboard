79f0856ac74986510c3b6dcb8170de4cf10abee9
Monika Gill <meenugill2603@gmail.com>
2025-04-29 21:13:32 +0530

    change db in setting file

--- gst_dashboard/settings.py
+++ gst_dashboard/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'gst_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gst_dashboard',
-        'USER': 'admin',
-        'PASSWORD': 'Arjun@moto26',
-        'HOST': '72.167.40.82',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'gst_dashboard',
+#         'USER': 'admin',
+#         'PASSWORD': 'Arjun@moto26',
+#         'HOST': '72.167.40.82',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
