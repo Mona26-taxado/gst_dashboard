59dcdbca0495f495b51590eecd3551a9bafbd07d
Monika Gill <meenugill2603@gmail.com>
2025-12-30 17:58:56 +0530

    add CSRF_TRUSTED

--- gst_dashboard/settings.py
+++ gst_dashboard/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7!vt#&t0m+#hhx*lo_@@(a5bp27^*$hum$jb$y#rw(_gm@0p&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
